cafe427362c2906f88215e8ad1e4652be4132cb5
unai-garciagi <unai.garciagi@elorrieta-errekamari.com>
2021-02-10 11:50:30 +0100

    Arreglado el test de Modelo

--- CineElorrietaMVC/src/test/java/TestModelo/ModeloTest.java
+++ CineElorrietaMVC/src/test/java/TestModelo/ModeloTest.java
@@ -11,17 +11,4 @@ public class ModeloTest {
 	
 	private Modelo modelo = new Modelo();
 
-	@Test
-	public void testUsuarioLocal() {
-		modelo.usuarioLocal("Uno", "Hola", 'r');
-		String usuario = modelo.getUsuario();
-		String local = modelo.getLocal();
-		char tipoLocal = modelo.getTipoLocal();
-		String tipoLocalString = modelo.getTipoLocalCompleto();
-		assertEquals("Uno", usuario);
-		assertEquals("Hola", local);
-		assertEquals('r', tipoLocal);
-		assertEquals("Restaurante", tipoLocalString);
-	}
-
 }
