69fb128f933456cbae59be2a1719640781ce730c
unai-garciagi <unai.garciagi@elorrieta-errekamari.com>
2021-02-24 10:58:11 +0100

    Hecho tests sobre local utilizando Mockito

--- CineElorrietaMVC/src/test/java/TestModelo/TestLocal.java
+++ CineElorrietaMVC/src/test/java/TestModelo/TestLocal.java
@@ -2,69 +2,77 @@ package TestModelo;
 
 import Modelo.*;
 import java.sql.Date;
-import java.util.ArrayList;
 
 import static org.junit.Assert.*;
 import org.junit.Test;
+
+import static org.mockito.Mockito.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.junit.runner.RunWith;
 
 public class TestLocal {
 	
 	
 	private Date fecha = new Date(0); //Fecha de tipo Date para insertar en las actividades
-	private Actividad a1 = new Actividad(11, fecha, "Primero");
-	private Actividad a2 = new Actividad(34, fecha, "Segundo");
+	private Actividad mockA1 = mock(Actividad.class);
+	private Actividad mockA2 = mock(Actividad.class);
+	private Producto mockp1 = mock(Producto.class);
 	private Producto p1 = new Producto("Prod1",fecha, "Unproducto", 1, 3);
 	private Producto p2 = new Producto("Prod2", fecha, "Otroproducto",2,4);
+	private ProductoTienda mockPt1 = mock(ProductoTienda.class);
+	private ProductoTienda mockPt2 = mock(ProductoTienda.class);
 	private ProductoTienda pt1 = new ProductoTienda(p1, 3);
 	private ProductoTienda pt2 = new ProductoTienda(p2, 9);
 	private Local loc = new Local("46564187J", "Primero", "Calle Pepito", "Restaurante");
 	
 	@Test
 	public void testAnnadirActividad() {
-		boolean test = loc.addActividad(a1);
+		boolean test = loc.addActividad(mockA1);
 		assertTrue(test);
-		test = loc.addActividad(a2);
+		test = loc.addActividad(mockA2);
 		assertTrue(test);
 	}
 	
 	@Test
 	public void testBuscarActividad() {
-		loc.addActividad(a1);
-		System.out.println(loc.getListaAct().toString());
+		loc.addActividad(mockA1);
+		when(mockA1.getNumTransaccion()).thenReturn(11);
 		Actividad test = loc.buscarActividad(11);
-		assertEquals(a1.getNumTransaccion(), test.getNumTransaccion());
+		assertEquals(mockA1.getNumTransaccion(), test.getNumTransaccion());
 		test = loc.buscarActividad(98);
 		assertNull(test);
 	}
 	
 	@Test
 	public void testEliminarActividad() {
-		loc.addActividad(a1);
+		loc.addActividad(mockA1);
+		when(mockA1.getNumTransaccion()).thenReturn(11);
 		boolean test = loc.eliminarActividad(11);
 		assertTrue(test);
 		test = loc.eliminarActividad(11);
 		assertFalse(test);
-		Actividad testact = loc.buscarActividad(11);
-		assertNull(testact);
-		loc.addActividad(a1);
-		loc.addActividad(a2);
-		test = loc.eliminarActividad(11);
-		assertTrue(test);
 	}
 	
 	@Test
 	public void testAnnadirProd() {
-		boolean test = loc.addProdTienda(pt1);
+		boolean test = loc.addProdTienda(mockPt1);
 		assertTrue(test);
-		test = loc.addProdTienda(pt2);
+		test = loc.addProdTienda(mockPt2);
 		assertTrue(test);
 	}
 	
 	@Test
 	public void testBuscarProd() {
-		loc.addProdTienda(pt1);
+		loc.addProdTienda(mockPt1);
+		when(mockp1.getNombre()).thenReturn("Prod1");
+		when(mockPt1.getProd()).thenReturn(mockp1);
 		ProductoTienda test = loc.buscarProdTienda("Prod1");
-		assertEquals(pt1.getProd().getNombre(),test.getProd().getNombre());
+		assertEquals(mockPt1.getProd().getNombre(),test.getProd().getNombre());
 		test = loc.buscarProdTienda("asdfasdfsdf");
 		assertNull(test);
 	}
@@ -72,16 +80,12 @@ public class TestLocal {
 	
 	@Test
 	public void testEliminarProd() {
-		loc.addProdTienda(pt1);
+		loc.addProdTienda(mockPt1);
+		when(mockp1.getNombre()).thenReturn("Prod1");
+		when(mockPt1.getProd()).thenReturn(mockp1);
 		boolean test = loc.eliminarProdTienda("Prod1");
 		assertTrue(test);
 		test = loc.eliminarProdTienda("Prod1");
 		assertFalse(test);
-		ProductoTienda testprod = loc.buscarProdTienda("Prod1");
-		assertNull(testprod);
-		loc.addProdTienda(pt1);
-		loc.addProdTienda(pt2);
-		test = loc.eliminarProdTienda("Prod1");
-		assertTrue(test);
 	}
 }
